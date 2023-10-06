8562d4e1e9905699f20bb94513987ef46324a276
noeflandre <noeflandre@localhost>
2023-10-06 15:11:12 +0200

    Created the constructor of the class

--- CalculatriceNotationPolonaiseInverse/src/Model/CalculatorModel.java
+++ CalculatriceNotationPolonaiseInverse/src/Model/CalculatorModel.java
@@ -3,6 +3,10 @@ package Model;
 import java.util.Stack;
 
 public class CalculatorModel {
+	// Attribute representing the stack of the calculator
 	private Stack<Double> stack;
     
+	public CalculatorModel() {
+        // Constructor of the class CalculatorModel
+    }
 }
