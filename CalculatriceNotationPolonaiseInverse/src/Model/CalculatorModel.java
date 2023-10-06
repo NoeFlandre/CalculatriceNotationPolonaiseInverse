dfb001d0eb1d006c2419800ebd8eb57f89c5d162
noeflandre <noeflandre@localhost>
2023-10-06 15:14:35 +0200

    Created a method push

--- CalculatriceNotationPolonaiseInverse/src/Model/CalculatorModel.java
+++ CalculatriceNotationPolonaiseInverse/src/Model/CalculatorModel.java
@@ -6,7 +6,14 @@ public class CalculatorModel {
 	// Attribute representing the stack of the calculator
 	private Stack<Double> stack;
     
-	public CalculatorModel() {
-        // Constructor of the class CalculatorModel
+	// Constructor of the class CalculatorModel
+	public CalculatorModel() {		
+		stack = new Stack<>();
+    }
+	
+	// Method to add an element in the stack
+    public void push(double value) {
+    	stack.push(value);
+        
     }
 }
