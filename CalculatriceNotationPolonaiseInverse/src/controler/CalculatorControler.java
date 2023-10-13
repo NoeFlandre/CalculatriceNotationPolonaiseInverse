5a2c3e5d62f25563efc2087ff3a1a7586e32364e
noeflandre <noeflandre@localhost>
2023-10-13 14:47:42 +0200

    Created a CalculatorControler class implementing the CalculatorControlerInterface interface

---
+++ CalculatriceNotationPolonaiseInverse/src/controler/CalculatorControler.java
@@ -0,0 +1,21 @@
+package controler;
+
+import java.util.Stack;
+import Model.CalculatorModel;
+import Model.CalculatorModelInterface;
+
+public class CalculatorControler implements CalculatorControlerInterface {
+
+	@Override
+	public void change(String accu) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void change(Stack<Double> stackData) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
