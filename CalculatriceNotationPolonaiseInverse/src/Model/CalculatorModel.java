15f4f65ca61823372021f5f54287f73d6d9a7f33
noeflandre <noeflandre@localhost>
2023-10-13 14:30:02 +0200

    Created a CalculatorModelInterface interface implemented by the CalculatorModel class

--- CalculatriceNotationPolonaiseInverse/src/Model/CalculatorModel.java
+++ CalculatriceNotationPolonaiseInverse/src/Model/CalculatorModel.java
@@ -2,7 +2,7 @@ package Model;
 
 import java.util.Stack;
 
-public class CalculatorModel {
+public class CalculatorModel implements CalculatorModelInterface {
 	// Attribute representing the stack of the calculator
 	private Stack<Double> stack;
     
